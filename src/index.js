2bcea771940578a02ecbbbc7ee7ec5f8367980f1
Rahul Jain <rahuljait@gmail.com>
2021-07-26 16:48:19 +0530

    Changed the functionality added Bootstrap

--- src/index.js
+++ src/index.js
@@ -21,31 +21,50 @@
 
   function createQuestion(questionPrompt, options, correctOptionIndex) {
     const questionContainer = document.createElement("div");
-  
+    questionContainer.setAttribute("id","questionscontainer");
     const prompt = document.createElement("h2");
+    prompt.setAttribute("class","text-center pt-3");
     prompt.appendChild(document.createTextNode(questionPrompt));
     questionContainer.appendChild(prompt);
+    let correctansdiv = document.createElement('div');
   
     const optionContainer = document.createElement("div");
-  
+    optionContainer.setAttribute("class","list-group");
     const correctOption = options[correctOptionIndex];
     console.log(correctOption);
-  
+    let attempt=0;
+    const alert = document.createElement("div");
+    let score =0;
     for (const option of options) {
-      const optionDiv = document.createElement("div");
+      const optionDiv = document.createElement("a");
+      optionDiv.setAttribute("class","list-group-item list-group-item-action");
+      const alertwrong="<strong>Wrong!</strong> Answer ";
+      const alertsuccess="<strong>Correct!</strong> Answer ";
       optionDiv.addEventListener("click", function () {
+        attempt++;
         if (correctOption === option) {
+          alert.setAttribute("class","alert alert-success alert-dismissible fade show mt-2");
+          alert.innerHTML=alertsuccess;
+          correctansdiv.appendChild(alert);
+          correctansdiv.style.order=1;
           alert(option + " is correct.");
         } else {
+          alert.setAttribute("class","alert alert-danger alert-dismissible fade show mt-2");
+          alert.innerHTML=alertwrong;
+          correctansdiv.appendChild(alert);
+          correctansdiv.style.order=1;
           alert(option + " is NOT correct.");
         }
       });
       optionDiv.textContent = option;
       optionContainer.appendChild(optionDiv);
     }
-  
+    
+    correctansdiv.className="hide";
+    correctansdiv.id="resultBox";
+    
     questionContainer.appendChild(optionContainer);
-  
+    questionContainer.appendChild(correctansdiv);
     return questionContainer;
   }
   
@@ -55,10 +74,13 @@
 
   // selection
   const appDiv = document.getElementById("app");
+  appDiv.setAttribute("style", "border: 1px solid red; border-radius: 15px;  padding: 39px; margin-bottom: 15px;");
   let length = questions.length;
   let prev = $("prevbtn");
   let next = $("nextbtn");
+  var rq =$("questionscontainer");
   let qid=0;
+  
   window.addEventListener("load",()=>{
     const questionElement1 = createQuestion(
       questions[qid].question,
@@ -69,29 +91,38 @@
     
   });
   next.addEventListener("click",function(){
-    
-    qid = qid+1;
-    const questionElement2 = createQuestion(
-      questions[qid].question,
-      questions[qid].answers,
-      questions[qid].correctIndex
-    );
-    appDiv.append(questionElement2);
-
-  });
-  prev.addEventListener("click",function(){
-    if(qid ==0){
-      prev.setAttribute("class","disabled");
-      return;
-    } else{
-      qid = qid-1;
-      const questionElement2 = createQuestion(
+      qid = qid+1;
+      if(qid === length-1)
+        next.classList.add("disabled")
+      if(prev.classList.contains("disabled"))
+        prev.classList.remove("disabled");
+      document.getElementById("questionscontainer").remove();
+      // alert.remove();
+      
+      const questionElement1 = createQuestion(
         questions[qid].question,
         questions[qid].answers,
         questions[qid].correctIndex
       );
-    }
-    appDiv.removeChild(questionElement2);
+      appDiv.append(questionElement1);
+    
+
+  });
+  prev.addEventListener("click",function(){
+    
+    qid = qid-1;
+    if(qid === 0)      
+      prev.classList.add("disabled");
+      document.getElementById("questionscontainer").remove();
+     
+      const questionElement1 = createQuestion(
+        questions[qid].question,
+        questions[qid].answers,
+        questions[qid].correctIndex
+      );
+      appDiv.appendChild(questionElement1);
+    
+    
   });
   // for (const questionObj of questions) {
   //   const questionElement = createQuestion(
